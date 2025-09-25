278ddfdfba12ca0886a236add91ad2d36df061ef
Richard Phan <richard.phan626@gmail.com>
2025-09-25 12:16:33 -0700

    Add files via upload
    Includes new features, such as box size, qr code color, background color

---
+++ main2.py
@@ -0,0 +1,14 @@
+import qrcode
+
+qr = qrcode.QRCode(version=1,
+                    error_correction=qrcode.constants.ERROR_CORRECT_H,
+                    box_size=10, #adjust size of qr code
+                    border=2)
+
+qr.add_data("https://www.youtube.com/watch?v=IX0RvTEVtDw") #insert whatever link you want
+qr.make(fit=True)
+
+img = qr.make_image(fill_color ="red", back_color ="black") #swap colors with the ones you want
+img.save("coloredCode.png")
+img.show()
+
